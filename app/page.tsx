2d054f7ac59edc8cc1b9134ab03fbf7d6369355f
PM-Manager <niiyama@rokubunnoni.com>
2025-12-12 15:18:22 +0900

    文言をアップデート

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>CandyDrops クローン開発中！</h1>
-      <p>PMの私がデプロイまで担当します。</p>
+      <h1>I updated the contents! </h1>
+      <p>I AM THE 3RD ENGINEER.</p>
     </div>
   );
 }
